cfead63ec6cc54f3b767dcfcef4b05984e178014
Proweeb <110546008+Proweeb@users.noreply.github.com>
2025-02-24 18:22:53 +0100

    testing out Toast

--- GWL/app/app.js
+++ GWL/app/app.js
@@ -9,10 +9,12 @@ import { database } from "../database/database";
 import { styles } from "../components/styles";
 import * as Notifications from "expo-notifications";
 import { LogBox } from "react-native";
+import Toast, { BaseToast } from "react-native-toast-message";
 
 import ScanScreen from "./scan/scan";
 import BottomTabNavigator from "./tabs/_layout";
 import AktionenNavigator from "./actions/_layout";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 // Create Stack & Tab Navigators
 const Stack = createStackNavigator();
@@ -20,6 +22,22 @@ LogBox.ignoreLogs([
   "Non-serializable values were found in the navigation state",
 ]);
 
+const toastConfig = {
+  success: (props) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: styles.green }}
+      contentContainerStyle={{ backgroundColor: "white" }}
+      text2Style={{
+        fontSize: RFPercentage(1.5),
+      }}
+      text1Style={{
+        fontSize: RFPercentage(2.5),
+        fontWeight: "400",
+      }}
+    />
+  ),
+};
 export default function App() {
   useEffect(() => {
     SystemUI.setBackgroundColorAsync(styles.backgroundColor);
@@ -49,14 +67,27 @@ export default function App() {
       // });
     };
     a();
+
+    const showToast = () => {
+      Toast.show({
+        type: "success",
+        text1: "Hello",
+        text2: "This is some something 👋",
+        position: "bottom",
+      });
+    };
+    showToast();
   }, []);
 
   return (
-    <DatabaseProvider database={database}>
-      <NavigationContainer>
-        <MainStack />
-      </NavigationContainer>
-    </DatabaseProvider>
+    <>
+      <DatabaseProvider database={database}>
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
+      </DatabaseProvider>
+      <Toast config={toastConfig} />
+    </>
   );
 }
 
