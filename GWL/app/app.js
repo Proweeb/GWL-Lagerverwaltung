e08b146cb6011ab5640fbe1b449a06ea365b3cff
Proweeb <110546008+Proweeb@users.noreply.github.com>
2025-02-28 23:51:26 +0100

    cleaned

--- GWL/app/app.js
+++ GWL/app/app.js
@@ -9,12 +9,13 @@ import { database } from "../database/database";
 import { styles } from "../components/styles";
 import * as Notifications from "expo-notifications";
 import { LogBox } from "react-native";
-import Toast, { BaseToast } from "react-native-toast-message";
+import Toast from "react-native-toast-message";
 
 import ScanScreen from "./scan/scan";
 import BottomTabNavigator from "./tabs/_layout";
 import AktionenNavigator from "./actions/_layout";
-import { RFPercentage } from "react-native-responsive-fontsize";
+
+import { toastConfig } from "../components/toastConfig";
 
 // Create Stack & Tab Navigators
 const Stack = createStackNavigator();
@@ -22,26 +23,11 @@ LogBox.ignoreLogs([
   "Non-serializable values were found in the navigation state",
 ]);
 
-const toastConfig = {
-  success: (props) => (
-    <BaseToast
-      {...props}
-      style={{ borderLeftColor: styles.green }}
-      contentContainerStyle={{ backgroundColor: "white" }}
-      text2Style={{
-        fontSize: RFPercentage(1.5),
-      }}
-      text1Style={{
-        fontSize: RFPercentage(2.5),
-        fontWeight: "400",
-      }}
-    />
-  ),
-};
 export default function App() {
   useEffect(() => {
     SystemUI.setBackgroundColorAsync(styles.backgroundColor);
     NavigationBar.setBackgroundColorAsync(styles.backgroundColor);
+
     NavigationBar.setButtonStyleAsync("dark");
     requestNotifications(["alert", "sound"]).then(({ status, settings }) => {
       // …
@@ -54,29 +40,8 @@ export default function App() {
           shouldSetBadge: true,
         }),
       });
-
-      // Second, call scheduleNotificationAsync()
-      // await Notifications.scheduleNotificationAsync({
-      //   content: {
-      //     title: "Welcome Back",
-      //     sound: require("../assets/scanned.mp3"),
-      //     color: "#FF5733", // Notification accent color
-      //   },
-
-      //   trigger: null,
-      // });
     };
     a();
-
-    const showToast = () => {
-      Toast.show({
-        type: "success",
-        text1: "Hello",
-        text2: "This is some something 👋",
-        position: "bottom",
-      });
-    };
-    showToast();
   }, []);
 
   return (
